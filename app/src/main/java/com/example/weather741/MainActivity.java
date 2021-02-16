67a6fb64c38c55c5d164fcf829c67f7a2e0c8c43
Volodymyr Shkolin <vovashkolin115@gmail.com>
2021-02-16 16:47:37 +0200

    saving data when rotating the screen

--- app/src/main/java/com/example/weather741/MainActivity.java
+++ app/src/main/java/com/example/weather741/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.weather741;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,6 +24,8 @@ import com.example.weather741.recyclerview.RecyclerViewAdapter;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -30,11 +37,16 @@ public class MainActivity extends AppCompatActivity {
     RecyclerViewAdapter adapter;
     EditText etCity;
     Button btnSearch;
+    Parcelable state;
+    int count = 0;
+
+    String SAVED_RECYCLER_VIEW_STATUS_ID = "status_rv";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         date_dimeRV = findViewById(R.id.recyclerView);
         gridLayoutManager = new GridLayoutManager(getApplicationContext(), 2);
